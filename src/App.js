64a9d1c132023df7048a827382d4e9ba53ac80de
Victor Ingman <viregistermail@gmail.com>
2020-06-01 16:03:21 +0200

    more info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 import styled from 'styled-components'
 const JumboText = styled.p`
   text-transform: uppercase;
@@ -11,21 +9,60 @@ const JumboText = styled.p`
   font-weight: bolder;
 `
 
+const HashA = styled.a`
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif;
+  text-align: left;
+  padding-left: 2vh;
+  color: rgb(27, 149, 224);
+  font-weight: bold;
+`
+
+const A = styled.a`
+  font-family: courier, monospace;
+  text-align: left;
+  padding-left: 2vh;
+  color: blue;
+  font-weight: bold;
+`
+
+const AppWrapper = styled.div`
+  @media (orientation: portrait) {
+    transform: rotate(90deg) translateX(2vh);
+  }
+`
+
 const JumboWrapper = styled.div`
   & > ${JumboText} {
     font-size: ${props => 100 / props.lines || 25}vh;
+    @media (orientation: portrait) {
+      font-size: ${props => 100 / props.lines || 25}vw;
+    }
   }
+  
 `
 
 function App () {
   return (
-    <div className='App'>
+    <AppWrapper>
       <JumboWrapper lines={3}>
         <JumboText>black</JumboText>
         <JumboText>lives</JumboText>
         <JumboText>matter</JumboText>
       </JumboWrapper>
-    </div>
+      <HashA
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://twitter.com/hashtag/BlackLivesMatter'
+      >#BlackLivesMatter
+      </HashA>
+      <A
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://blacklivesmatters.carrd.co/'
+      >
+        how to help
+      </A>
+    </AppWrapper>
   )
 }
 
